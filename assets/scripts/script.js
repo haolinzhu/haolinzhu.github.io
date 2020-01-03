906f976bc617567803e2ef4726c421557a77a949
Haolin Zhu <haolinz@berkeley.edu>
2020-01-03 01:31:08 -0800

    simplify javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,62 +1,12 @@
 
-let portrait = document.getElementById("portrait");
-let about_description = document.getElementById("abtdes");
-let about_pic = document.getElementById("abtpic");
-portrait.onmouseover = function() {
-  about_description.style.display = "block";
-  about_pic.style.opacity = "1";
-}
-portrait.onmouseout = function() {
-  about_description.style.display = "none";
-  about_pic.style.opacity = "0.5";
-}
-
-let lights = document.getElementById("lights");
-let lights_description = document.getElementById("advdes");
-let lights_pic = document.getElementById("advpic");
-lights.onmouseover = function() {
-  lights_description.style.display = "block";
-  lights_pic.style.opacity = "1";
-}
-lights.onmouseout = function() {
-  lights_description.style.display = "none";
-  lights_pic.style.opacity = "0.5";
-}
-
-let code = document.getElementById("code");
-let code_description = document.getElementById("projdes");
-let code_pic = document.getElementById("projpic");
-code.onmouseover = function() {
-  code_description.style.display = "block";
-  code_pic.style.opacity = "1";
-}
-code.onmouseout = function() {
-  code_description.style.display = "none";
-  code_pic.style.opacity = "0.5";
-}
-
-let art = document.getElementById("art");
-let art_description = document.getElementById("artdes");
-let art_pic = document.getElementById("artpic");
-art.onmouseover = function() {
-  art_description.style.display = "block";
-  art_pic.style.opacity = "1";
-}
-art.onmouseout = function() {
-  art_description.style.display = "none";
-  art_pic.style.opacity = "0.5";
-}
-
-var lst = ["instagram", "lightpainting", "beach", "capital", "bug", "yosemite"];
+var lst = ["instagram", "lightpainting", "beach", "capital", "bug", "yosemite", "kaiping", "library"];
 var i;
 
 function hovering(i) {
   document.getElementById(lst[i]).onmouseover = function() {
         document.getElementById(lst[i] + "pic").style.opacity = "1";
   }
-}
 
-function nothovering(i) {
   document.getElementById(lst[i]).onmouseout = function() {
         document.getElementById(lst[i] + "pic").style.opacity = "0.5";
   }
@@ -64,25 +14,29 @@ function nothovering(i) {
 
 for (i = 0; i < lst.length; i ++) {
   hovering(i);
-  nothovering(i);
 }
 
-var lst_u = ["portrait", "code", "lights"];
+
+
+var lst_u = ["portrait", "code", "lights", "engineering", "art"];
 var j;
 
-function clicked(j) {
+function clicked_or_hover(j) {
   document.getElementById(lst_u[j]).onclick = function() {
         document.getElementById(lst_u[j] + "-text-container").scrollIntoView();
+  }
+
+  document.getElementById(lst_u[j]).onmouseover = function() {
+        document.getElementById(lst_u[j] + "des").style.display = "block";
+        document.getElementById(lst_u[j] + "pic").style.opacity = "1";
+  }
+
+  document.getElementById(lst_u[j]).onmouseout = function() {
+        document.getElementById(lst_u[j] + "des").style.display = "none";
+        document.getElementById(lst_u[j] + "pic").style.opacity = "0.5";
   }
 }
 
 for (j = 0; j < lst_u.length; j ++) {
-  clicked(j);
+  clicked_or_hover(j);
 }
-//document.getElementById("portrait").onclick = function() {
-//  document.getElementById("portrait-text-container").scrollIntoView();
-//}
-
-//document.getElementById("code").onclick = function() {
-//  document.getElementById("code-text-container").scrollIntoView();
-//}
