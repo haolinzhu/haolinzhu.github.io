464159d369c4b9bde35695cc701626fc71cd104b
Haolin Zhu <haolinz@berkeley.edu>
2020-08-12 00:27:15 -0700

    new image layout
    new image layout

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 
-var lst = ["instagram", "lightpainting", "beach", "capital", "bug", "yosemite", "kaiping", "library", "kaust"];
+var lst = ["instagram", "lightpainting", "beach", "capital", "bug", "yosemite", "kaiping", "library", "kaust", "dubai"];
 var i;
 
 function hovering(i) {
@@ -7,15 +7,14 @@ function hovering(i) {
         document.getElementById(lst[i] + "pic").style.filter = "none";
   }
 
-  document.getElementById(lst[i]).onmouseout = function() {
-        document.getElementById(lst[i] + "pic").style.filter = "grayscale(100%)";
-  }
+//   document.getElementById(lst[i]).onmouseout = function() {
+//         document.getElementById(lst[i] + "pic").style.filter = "grayscale(100%)";
+//   }
 }
 
 for (i = 0; i < lst.length; i ++) {
   hovering(i);
 }
-
 
 
 var lst_u = ["portrait", "code", "lights", "engineering", "art"];
@@ -24,12 +23,6 @@ var j;
 function clicked_or_hover(j) {
   document.getElementById(lst_u[j]).onclick = function() {
         document.getElementById(lst_u[j] + "-text-container").scrollIntoView();
-        // document.getElementById(lst_u[j]).style.position = "fixed";
-        // for (k = 0; k < lst_u.length; k ++) {
-        //   if (k != j) {
-        //     document.getElementById(lst_u[k]).style.position = "absolute";
-        //   }
-        // }
   }
 
   document.getElementById(lst_u[j]).onmouseover = function() {
@@ -39,7 +32,7 @@ function clicked_or_hover(j) {
 
   document.getElementById(lst_u[j]).onmouseout = function() {
         document.getElementById(lst_u[j] + "des").style.display = "none";
-        document.getElementById(lst_u[j] + "pic").style.filter = "grayscale(100%)";
+      //   document.getElementById(lst_u[j] + "pic").style.filter = "grayscale(100%)";
   }
 }
 
